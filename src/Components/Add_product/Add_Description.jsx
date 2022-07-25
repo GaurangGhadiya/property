902089f77b59b86b3785b93f0e55265ae743b3af
GaurangGhadiya <gaurangghadiya99@gmail.com>
2022-07-25 22:08:32 +0530

    Some Issue Solve

--- src/Components/Add_product/Add_Description.jsx
+++ src/Components/Add_product/Add_Description.jsx
@@ -4,9 +4,26 @@ import RichTextEditor from "react-rte";
 
 const Add_Description = () => {
     const [richValue, setrichValue] = useState(RichTextEditor.createEmptyValue());
+    const [richValue1, setrichValue1] = useState(RichTextEditor.createEmptyValue());
+    const [richValue2, setrichValue2] = useState(RichTextEditor.createEmptyValue());
+    const [pipData, setpipData] = useState({});
+    const [pipData1, setpipData1] = useState({});
+    const [pipData2, setpipData2] = useState({});
     const onChange = (value) => {
         setrichValue(value);
-    };
+        value.toString("html");
+        setpipData({ ...pipData, description: value.toString("html") });
+      };
+    const onChange1 = (value) => {
+        setrichValue1(value);
+        value.toString("html");
+        setpipData1({ ...pipData, description: value.toString("html") });
+      };
+    const onChange2 = (value) => {
+        setrichValue2(value);
+        value.toString("html");
+        setpipData2({ ...pipData, description: value.toString("html") });
+      };
     return (
         <div className='text_editor'>
             <div className="input_filed">
@@ -15,11 +32,11 @@ const Add_Description = () => {
             </div>
             <div className="input_filed">
                 <label className='text_editor_lable'>Column 2</label>
-                <RichTextEditor value={richValue} onChange={onChange} />
+                <RichTextEditor value={richValue1} onChange={onChange1} />
             </div>
             <div className="input_filed">
                 <label className='text_editor_lable'>Column 3</label>
-                <RichTextEditor value={richValue} onChange={onChange} />
+                <RichTextEditor value={richValue2} onChange={onChange2} />
             </div>
             <Grid container spacing={3}>
             <Grid item xs={12} sm={12} md={12}><h6 className='mt-4'>Supply Ability</h6></Grid>
